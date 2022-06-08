df693360dca3ff1db11bff334ba21851f9f34527
Joaovictormart <martinsvjoaos@gmail.com>
2022-06-08 15:21:28 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    public static void main (String[] args) {
+        
+        Login jLogin = new Login();
+        Catalogo jCatalogo = new Catalogo();
+        MainPrincipal jProduto = new MainPrincipal();
+        FinCompra jFinCompra = new FinCompra();
+        
+        
+        jCatalogo.setVisible(true);
+        
+        
+    }
+    
+}
